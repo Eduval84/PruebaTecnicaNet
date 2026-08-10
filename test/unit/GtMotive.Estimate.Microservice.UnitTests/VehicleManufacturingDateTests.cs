c0b026d1b14cf2bd64e245783bf9b6927c6a19c1
evaderrama <evalderrama@domingoalonsogroup.com>
2026-08-10 10:59:07 +0100

    test(domain): add failing test for manufacturing date older than five years

---
+++ test/unit/GtMotive.Estimate.Microservice.UnitTests/VehicleManufacturingDateTests.cs
@@ -0,0 +1,22 @@
+using System;
+using FluentAssertions;
+using GtMotive.Estimate.Microservice.Domain;
+using Xunit;
+
+namespace GtMotive.Estimate.Microservice.UnitTests;
+
+public class VehicleManufacturingDateTests
+{
+    [Fact]
+    public void CreateShouldThrowDomainExceptionWhenDateIsOlderThanFiveYears()
+    {
+        // Arrange
+        var invalidManufacturingDate = DateOnly.FromDateTime(DateTime.UtcNow.Date.AddYears(-6));
+
+        // Act
+        Action act = () => ManufacturingDate.Create(invalidManufacturingDate);
+
+        // Assert
+        act.Should().Throw<DomainException>();
+    }
+}
